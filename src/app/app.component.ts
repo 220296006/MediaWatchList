1dfc7f1ba11eaa41cf642f6aecee9e6338b51a98
Thabiso Matsaba <you@example.com>
2022-12-05 17:46:13 +0200

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app' ;
+  title2 = '<p>Keep track of the media I watch</p>'
+}
