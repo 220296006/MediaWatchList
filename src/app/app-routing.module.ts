5c22bdebacbb5469dd34143b5d3ab47a1ca4a9e8
Thabiso Matsaba <you@example.com>
2022-12-28 17:45:12 +0200

    made changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule, ɵgetHostElement } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { MediaItemComponent } from './features/media/media-item/media-item.component';
+import { MovieFormComponent } from './movie-form/movie-form.component';
+import { NavComponent } from './shared/components/nav/nav.component';
 
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', component: HomeComponent }
+  {path: 'nav', pathMatch: 'full', component: HomeComponent },
+  {path:'movie-form', pathMatch: 'full', component: MovieFormComponent },
+  {path: 'media-item', pathMatch: 'full', component: MediaItemComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularMaterialModule } from './modules/material.module';
 
 import { MediaItemComponent } from './features/media/media-item/media-item.component';
 import { HomeComponent } from './features/home/home.component';
 import { ImageComponent } from './features/image/image.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MovieFormComponent } from './movie-form/movie-form.component';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { NavComponent } from "./shared/components/nav/nav.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MediaItemComponent,
-    HomeComponent,
-    ImageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MediaItemComponent,
+        HomeComponent,
+        ImageComponent,
+        MovieFormComponent,
+        NavComponent
+    ],
+    providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } }],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        AngularMaterialModule,
+        FormsModule,
+        
+    ]
 })
 export class AppModule { }
